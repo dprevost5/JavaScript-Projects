3ae5b132c40367b19b0420ea5616f1b0da872ef4
dprevost5 <devonprevost@gmail.com>
2021-03-26 13:23:17 -0400

    Create main.js
    Create main.js

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function My_First_Function() {   //create function
+    var str = "I love coffee!";  //assign first variable
+    var result = str.fontcolor("brown");  //assign second variable
+    document.getElementById("coffee").innerHTML = result;  //use document.getElementById method to return the element with ID attribute "coffee"
+}
+
+function myFunction() {     //create function
+    var sentence = "I like to drink coffee";  //assign string value to variable
+    sentence += " in the morning.";  //concatenate the string
+    document.getElementById("concatenate").innerHTML = sentence;  //use document.getElementById method to return element with ID attribute "concatenate"
+}
