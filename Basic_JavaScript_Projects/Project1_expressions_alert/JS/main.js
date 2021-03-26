8cd3bf2434656cc76072c06c641c13c3f3572e1a
dprevost5 <devonprevost@gmail.com>
2021-03-26 11:43:32 -0400

    Create main.js
    Create main.js

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,27 @@
+window.alert("Hello, World!");
+
+document.write("Hello, World!");
+
+var A = "This is a string.";  //assigning string value to a variable
+document.write(A);     //using document.write() method
+
+var B = "Watch out!";  //assign string value to a variable
+window.alert(B);      //using window.alert() method
+
+document.write("The sign read \"Danger! Black Ice\"");
+
+document.write("Get your ice skates, " + "it's time for some fun!"); //concatenating two strings
+
+var familyPets = "4", Dog = "Spot", Cat = "Meow Meow", Bird = "Jewels", Gerbil = "Henry";
+document.write(Cat);
+
+var A = A.fontcolor("blue");  //assinging a color to a variable
+document.write(A);
+var B = B.fontcolor("red");  //assigning a color to a variable
+document.write(B);
+var familyPets = familyPets.fontcolor("orange");  //assigning a color to a variable
+document.write(familyPets);
+
+var C = "We have a lot of pets ";  //assign string to a variable
+var D = "we love."                 //assign string to a variable
+document.write(C+D);               //concatenate the variables
